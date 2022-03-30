dc129b8cc80e00dd30e9e0ab3e0ce91a711ab3aa
Tomas's Home <tomasmarmay21@gmail.com>
2022-03-30 14:26:03 -0300

    ej3 done

--- lab1/ej3/array_helpers.h
+++ lab1/ej3/array_helpers.h
@@ -1,1 +1,7 @@
-void add(int a, int b);
+void print_help(char *program_name);
+
+char *parse_filepath(int argc, char *argv[]);
+
+unsigned int array_from_file(int array[],unsigned int max_size,const char *filepath);
+
+void array_dump(int a[], unsigned int length);
--- lab1/ej3/main.c
+++ lab1/ej3/main.c
@@ -3,10 +3,23 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#define MAX_SIZE 100000
 
-int main(void){
-    int a,b;
-    a = 2;
-    b = 3;
-    add(a,b);
+int main(int argc, char *argv[]) {
+
+    char *filepath = NULL;
+
+    /* parse the filepath given in command line arguments */
+    filepath = parse_filepath(argc, argv);
+    
+    /* create an array of MAX_SIZE elements */
+    int array[MAX_SIZE];
+    
+    /* parse the file to fill the array and obtain the actual length */
+    unsigned int length = array_from_file(array, MAX_SIZE, filepath);
+    
+    /*dumping the array*/
+    array_dump(array, length);
+    
+    return EXIT_SUCCESS;
 }
