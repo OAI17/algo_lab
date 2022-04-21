ff10a346cf1fdadbc94305627b48f3878f798c94
Tomas's Home <tomasmarmay21@gmail.com>
2022-04-21 15:26:52 -0300

    working on ej0

--- lab03/ej0/main.c
+++ lab03/ej0/main.c
@@ -12,19 +12,55 @@ static void dump(char a[], unsigned int length) {
     printf("\n\n");
 }
 
-int main(/* -- completar-- */) {
+
+unsigned int data_from_file(const char *path,unsigned int indexes[],char letters[],unsigned int max_size){
+        path = fopen(*path,"r");
+        if (path != NULL){
+            int elem=max_size;
+            int unsigned j=0;
+            char p;
+            
+            char p;
+            //fscanf devuelve EOF cuando se alcanza el final de la entrada
+            while(fscanf(*path, "%c", p) != E0F){
+                printf("%s \n",p);
+                  
+                //indexes[j] = agrege al indexes los numeros
+                //letters[j] = agrege al letters las letras
+                j = j+1;
+                elem = elem -1; 
+                
+                
+            }
+            fclose(path);
+            elem = max_size - (max_size-elem);
+            //asigne el ultimo i a una variable que va a ser la que devuelvo (max_size-indexes[ultima posicion])
+            return elem;
+        }
+        else{
+            printf("Error abriendo el archivo \n");
+            return EXIT_FAILURE;
+        }
+    }
+
+int main(int argc, char *argv[]) {
     FILE *file;
     unsigned int indexes[MAX_SIZE];
     char letters[MAX_SIZE];
-    char sorted[MAX_SIZE];
+    //char sorted[MAX_SIZE];
     unsigned int length=0; 
+    
+    
     //  .----------^
     //  :
     // Debe guardarse aqui la cantidad de elementos leidos del archivo
     
     /* -- completar -- */
 
-    dump(sorted, length);
+
+    length = data_from_file(file,indexes,letters,MAX_SIZE);
+
+    //dump(sorted, length); 
 
     return EXIT_SUCCESS;
 }
